8d3bc640fde1d388902c0e2a638dc91ec54e33cb
Vivek Singla <singlavivek2005@gmail.com>
2024-01-29 19:23:43 +0530

    issue fixed

--- controllers/addTaskController.js
+++ controllers/addTaskController.js
@@ -1,16 +1,30 @@
-const pool = require("../dbConfig")
+const pool = require("../dbConfig");
+
+const successObject = {
+  success: true,
+  message: "Task successfully added to the database.",
+};
+const databaseError = {
+  success : false,
+  error: {
+    code: "ERR_DATABASE",
+    message: "Failed to add task to the database. Please try again later."
+  }
+}
+
 
 const addTask = (req, res) => {
+  const date= new Date();
+  const dateString=date.toString();
   let taskName = req.body.taskName;
-  let sql = `INSERT INTO todos (name) VALUES ("${taskName}");`;
+  let sql = `INSERT INTO todos (name,created_date,updated_date) VALUES ("${taskName}","${dateString}","${dateString}");`;
   pool.query(sql, [taskName], (err, result, fields) => {
-    if (err) throw err;
-    else
-      res.send({
-        success: true,
-        message: "Task successfully added to the database.",
-      });
+    if (err) 
+    {res.status(500).json(databaseError);
+      console.log(err);
+    }
+    else res.send(successObject);
   });
 };
 
-module.exports=addTask;
+module.exports = addTask;
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-const addTask = require("./controllers/addTaskController")
+const addTask = require("./controllers/addTaskController");
+const showAllTasks=require("./controllers/showAllTaskController");
+const deleteTask = require("./controllers/deleteTaskController");
+const updateTask = require ("./controllers/updatetaskController");
 
 const app=express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/addTask", addTask);
+app.post("/tasks", addTask);
+app.get("/tasks",showAllTasks);
+app.delete("/tasks/",deleteTask);
+app.patch("/tasks",updateTask)
 
 app.listen(3000,()=>{
   console.log("Server on");
