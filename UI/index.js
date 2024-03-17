560ac3417bf5412dd3fec3fae2e3dc77aca5ef3c
Vivek Singla <singlavivek2005@gmail.com>
2024-03-17 11:23:55 +0530

    made dateTime function,createdActionBtn function and cleaned the code

--- UI/index.js
+++ UI/index.js
@@ -31,39 +31,42 @@ let todoList = [
 
 function convertDate(dateString) {
   const dateObject = new Date(dateString);
-  // console.log(`${String(dateObject.getDate)}`);
-  return `${dateObject.getMonth()+1}-${dateObject.getDate()}-${dateObject.getFullYear()}`;
+  return dateObject.toLocaleDateString(undefined);
+}
+
+function createActionBtns() {
+  let tableCell = document.createElement("td");
+  let tickButton = document.createElement("button");
+  let deleteButton = document.createElement("button");
+  tickButton.textContent = "✅";
+  deleteButton.textContent = "❌";
+  tableCell.appendChild(tickButton);
+  tableCell.appendChild(deleteButton);
+  return tableCell;
 }
 
 function displayTodo(list) {
   const tableBody = document.getElementById("table-list");
-  for (let i = 0, k = "1"; i < list.length; i++, k++) {
+  for (let i = 0; i < list.length; i++) {
     let tableRow = document.createElement("tr");
     const objectKeys = Object.keys(list[i]);
     let td = document.createElement("td");
-    td.textContent = k;
+    td.textContent = i + 1;
     tableRow.appendChild(td);
     for (let j = 0; j < objectKeys.length; j++) {
       if (objectKeys[j] === "id") continue;
-      let tableData = document.createElement("td");
+      let tableCell = document.createElement("td");
       if (
         objectKeys[j] === "created_date" ||
         objectKeys[j] === "updated_date"
       ) {
-        tableData.textContent = convertDate(list[i][objectKeys[j]]);
+        tableCell.textContent = convertDate(list[i][objectKeys[j]]);
       } else {
-        tableData.textContent = list[i][objectKeys[j]];
+        tableCell.textContent = list[i][objectKeys[j]];
       }
-      tableRow.appendChild(tableData);
+      tableRow.appendChild(tableCell);
     }
-    let tableData = document.createElement("td");
-    let tickButton = document.createElement("button");
-    let deleteButton = document.createElement("button");
-    tickButton.textContent = "✅";
-    deleteButton.textContent = "❌";
-    tableData.appendChild(tickButton);
-    tableData.appendChild(deleteButton);
-    tableRow.appendChild(tableData);
+    tableRow.appendChild(createActionBtns());
     tableBody.appendChild(tableRow);
   }
 }
